0d8206fbff56428dbb83e2e6f75063a561900d96
ninjapretzel <ninjapretzel@yahoo.com>
2013-10-14 01:19:21 -0600

    adds some more overloads for button, adds
    GUIElement framework

---
+++ Classes/System/ZScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class ZScript {
+	public static Table values;
+	public static Table flags;
+
+	public static void SetFlag(string name) { flags[name] = 1; }
+	public static void UnsetFlag(string name) { flags.Remove(name); }
+	public static bool GetFlag(string name) { return (flags[name] == 1); }
+	
+	public static void SetValue(string name, float value) { values[name] = value; }
+	public static float GetValue(string name) { return values[name]; }
+	
+	
+	
+	
+}
