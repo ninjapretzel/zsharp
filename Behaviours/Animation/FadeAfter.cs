6b115bfec5291d586dfcb1d6a3f7a56b9d067621
ninjapretzel <ninjapretzel@yahoo.com>
2014-03-27 19:23:40 -0600

    Adds Timer and Message classes, FadeAfter...
    behaviour to fade something after a delay.
    
    The plan is to work the timer into all the classes that need it.
    
    Timers will be able (eventually) to check Game.paused in order to
    update, and will have an option to tick even when paused.
    
    The Message class stores a string, and can be drawn on the screen. Has
    support for text colors, using escape codes inside of the string (ex.
    "\\r" or literally (as it would appear normally, \r) to change the text
    color to red.)
    Multiple text codes are supported, and support for more will be added in
    the future.

---
+++ Behaviours/Animation/FadeAfter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeAfter : MonoBehaviour {
+	
+	public Timer timer = new Timer(3);
+	public float fadeTime = .5f;
+	
+	void Update() {
+		if (timer.Update()) {
+			transform.FadeOut(fadeTime);
+		}
+	}
+	
+}
---
+++ Classes/GUI/Message.cs
@@ -0,0 +1,161 @@
+﻿using UnityEngine;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Message {
+	
+	const int SPACING = 4;
+	
+	public string str;
+	public static Dictionary<char, Color> colorMap;
+	
+	float width = 0;
+	float usedThisLine = 0;
+	Vector2 size;
+	
+	static Color C(double r, double g, double b) { return new Color((float)r, (float)g, (float)b); }
+	static Color C(double d) { float f = (float)d; return new Color(f, f, f); }
+	
+	static Message() {
+		colorMap = new Dictionary<char, Color>();
+		colorMap.Add('r', Color.red );
+		colorMap.Add('o', C(1, .75, 0) );
+		colorMap.Add('y', Color.yellow );
+		colorMap.Add('g', Color.green );
+		colorMap.Add('b', Color.blue );
+		colorMap.Add('i', C(.8, .8, 1) );
+		colorMap.Add('v', C(.8, 0, 1) );
+		
+		colorMap.Add('c', Color.cyan );
+		
+		colorMap.Add('q', C(.8, .8, .8) );
+		colorMap.Add('e', C(1, .6, 0) );
+		colorMap.Add('t', C(.8, 1, .8) );
+		colorMap.Add('p', C(.8, 1, .8) );
+		
+		//colorMap.Add('e', new Color(1, .6f, .0f) );
+	}
+	
+	public Message() {
+		str = "";
+	}
+	
+	public Message(string s) {
+		str = s;
+	}
+	
+	public void Draw(Rect area) {
+		
+		width = area.Denormalized().width;
+		
+		GUILayout.BeginArea(area.Denormalized()); {
+		
+			
+			GUILayout.BeginVertical("box", GUILayout.ExpandHeight(false)); {
+				GUILayout.BeginHorizontal("label", GUILayout.ExpandHeight(false)); {
+				
+					int i = 0;
+					int safety = 0;
+					usedThisLine = 0;
+					
+					while (i < str.Length && safety++ < 100) {
+						
+						int nextEscape = str.IndexOf('\\', i);
+						int nextLine = str.IndexOf('\n', i);
+						if (nextLine == -1) { nextLine = str.Length + 10; }
+						if (nextEscape == -1) { nextEscape = str.Length + 10; }
+						
+						int pos = str.IndexOf(' ', i);
+						
+						int len = pos - i;
+						string s;
+						if (pos == -1) {
+							len = str.Length - i;
+						}
+						
+						if (nextEscape < nextLine) {
+							
+							if (nextEscape < pos) {
+								
+								if (nextEscape+1 < str.Length) {
+									char c = str[nextEscape+1];
+									s = str.Substring(i, nextEscape - i);
+									
+									Label(s);
+									
+									
+									ChangeColor(c);
+									
+								} else {
+									s = str.Substring(i, nextEscape - i);
+									Label(s);
+									
+								}
+								i = nextEscape + 2;
+								continue;
+							}
+							
+						} else if (nextLine < nextEscape) {
+						
+							if (nextLine < pos) {
+								s = str.Substring(i, nextLine - i);
+								Label(s);
+								Newline();
+								i = nextLine+1;
+								
+								
+								continue;
+							}
+							
+						}
+						
+						s = str.Substring(i, len);
+						Label(s);
+						
+						
+						if (pos != -1 && pos < str.Length) {
+							if (str[pos] == ' ') {
+								GUILayout.Space(SPACING);
+								usedThisLine += SPACING;
+							}
+						}
+						
+						i = pos+1;
+						if (pos == -1) { i = str.Length; }
+						
+					}
+					
+				} GUILayout.EndHorizontal();
+			} GUILayout.EndVertical();
+			
+		} GUILayout.EndArea();
+		
+		
+	}
+	
+	
+	
+	void Newline() {
+		GUILayout.EndHorizontal();
+		GUILayout.BeginHorizontal("label", GUILayout.ExpandHeight(false));
+		usedThisLine = 0;
+	}
+	
+	void ChangeColor(char c){
+		GUI.color = Color.white;
+		if (colorMap.ContainsKey(c)) {
+			GUI.color = colorMap[c];
+		}		
+	}
+	
+	void Label(string s) {
+		size = GUI.skin.label.CalcSize(new GUIContent(s));
+		if (usedThisLine + size.x >= width) { Newline(); }
+		
+		GUILayout.Label(s, GUILayout.ExpandWidth(false));
+		usedThisLine += size.x;
+	}
+	
+	
+}
