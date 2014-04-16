a0f77adc562c863c4d4d1edbc872a73b769c4156
MofoMan2000 <mofoman2000@gmail.com>
2014-04-15 19:08:09 -0600

    Add some useful references to GUIButtonAction class.

--- Behaviours/GUIBehaviours/GUIButtonAction.cs
+++ Behaviours/GUIBehaviours/GUIButtonAction.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class GUIButtonAction : MonoBehaviour {
 
 	public Transform realParent { get { return transform.parent.parent; } set { transform.parent.parent = value; } }
+	public string text { get { if(textmesh==null) { return "null"; } return textmesh.text; } set { if(textmesh != null) { textmesh.text = value; } } }
 	
+	[System.NonSerialized] public TextMesh textmesh = null;
 	
 	void OnMouseUpAsButton() {
 		SendMessage("Action", SendMessageOptions.DontRequireReceiver);
@@ -21,35 +23,41 @@ public class GUIButtonAction : MonoBehaviour {
 		GameObject textObj = new GameObject("Text");
 		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
 		Transform t = cube.transform;
-		t.localScale = scale;
 		
 		button.layer = 8;
 		textObj.layer = 8;
 		cube.layer = 8;
 		
-		button.transform.position = position;
+		
 		textObj.transform.parent = button.transform;
 		t.parent = button.transform;
 		
-		cube.transform.localPosition = Vector3.zero;
 		textObj.transform.localPosition = Vector3.zero;
 		
-		MeshRenderer mr = textObj.AddComponent<MeshRenderer>();
-		TextMesh tm = textObj.AddComponent<TextMesh>();
+		
 		if (mat != null) {
 			cube.renderer.sharedMaterial = mat;
 		}
-			
-		mr.material = font.material;
+		
+		
+		MeshRenderer mr = textObj.AddComponent<MeshRenderer>();
+		TextMesh tm = textObj.AddComponent<TextMesh>();
+		
+		
+		button.transform.localPosition = position;
+		t.localScale = scale;
 		
 		tm.transform.localScale *= .15f;
 		tm.transform.localPosition = Vector3.forward * -.1f;
 		tm.font = font;
 		tm.anchor = TextAnchor.MiddleCenter;
 		tm.alignment = TextAlignment.Center;
+		mr.material = font.material;
 		tm.text = label;
+		cube.transform.localPosition = Vector3.zero;
 		
-		
-		return cube.AddComponent<T>();
+		T comp = cube.AddComponent<T>();
+		comp.textmesh = tm;
+		return comp;
 	}
 }
