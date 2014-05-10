d7c3e1742a5102c0428b5c02d70f004d52c88243
MofoMan2000 <mofoman2000@gmail.com>
2014-05-10 16:16:05 -0600

    Adds custom setting support.
    In order to use custom settings, a partial Settings class should be created and additional setting added as properties that use string indexes inside of the custom Table.
    
    Adds console GUISkin.

--- Classes/System/Settings.cs
+++ Classes/System/Settings.cs
@@ -11,23 +11,46 @@ public static class SettingsUtils {
 
 public static partial class Settings {
 	public static float overscanRatio = 0;
-	public static float musicVolume  = 1;
+	public static float musicVolume = 1;
 	public static float soundVolume = 1;
 	
+	//Custom settings can be stored in this table.
+	//This will be saved and loaded automatically.
+	public static Table custom;
+	
 	public static bool hints = true;
+	
+	static Settings() {
+		custom = new Table();
+		
+		
+		
+		
+	}
+	
 	
 	public static void Save() {
 		PlayerPrefs.SetFloat("set_overscan", overscanRatio);
 		PlayerPrefs.SetFloat("set_musicVolume", musicVolume);
 		PlayerPrefs.SetFloat("set_soundVolume", soundVolume);
 		
+		custom.Save("set_custom");
+		
 		PlayerPrefsF.SetBool("set_hints", hints);
 	}
 	
 	public static void Load() {
+		if (!PlayerPrefs.HasKey("set_musicVolume")) {
+			musicVolume = .5f;
+			soundVolume = .5f;
+			overscanRatio = 0;
+			return;
+		}
 		overscanRatio = PlayerPrefs.GetFloat("set_overscan");
 		musicVolume = PlayerPrefs.GetFloat("set_musicVolume");
 		soundVolume = PlayerPrefs.GetFloat("set_soundVolume");
+		
+		custom.Load("set_custom");
 		
 		hints = PlayerPrefsF.GetBool("set_hints");
 		
