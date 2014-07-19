c709fc685665fde5c7d8a7e76290dd0db65ba889
ninjapretzel <ninjapretzel@yahoo.com>
2014-07-19 14:04:22 -0600

    Adds ParticleMaker and MassParticles classes
    ParticleMaker has a virtual property 'particle' that returns a
    ParticleSystem.Particle. This is to be overridden to create custom
    particle animations.
    
    MassParticles has an array of ParticleMakers that it passes over in
    order to fill a ParticleSystem every Update()

---
+++ Classes/Structs/ParticleMaker.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ParticleMaker {
+
+	public Vector3 position = Vector3.zero;
+	public float size = 100;
+	public Color color = Color.white;
+	public float angularVelocity = 0;
+	public float rotation = 0;
+	
+	public virtual ParticleSystem.Particle particle {
+		get {
+			ParticleSystem.Particle p = new ParticleSystem.Particle();
+			
+			p.position = position;
+			p.size = size;
+			p.color = color;
+			p.rotation = rotation + angularVelocity * Time.time;
+			
+			return p;
+		}
+	}
+	
+}
