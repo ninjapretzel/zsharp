cabc637aa83210ec26fb7c0b1b532ae70715f8bd
ninjapretzel <ninjapretzel@yahoo.com>
2014-08-03 22:18:21 -0600

    Modifies Flags class
    Flags is now Dictionary<string, bool> rather than Dictionary<int, bool>.
    Size of save is slightly expanded, saving true keys as comma-separated
    list.
    
    changes the indexer property to return false if the key does not exist.

--- Classes/Data/Flags.cs
+++ Classes/Data/Flags.cs
@@ -5,85 +5,52 @@ using System.Reflection;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Flags : Dictionary<int, bool> {
+public class Flags : Dictionary<string, bool> {
 	
-	public int maxIndex { get { 
-		int highest = -1;
-		foreach (int i in Keys) {
-			if (i > highest) { highest = i; }
-		}
-		return highest;
-	}}
+	public Flags() : base() { }
+	public Flags(string str) : base() { LoadString(str); }	
 	
-	public new bool this[int i] {
-		get { 
-			if (ContainsKey(i)) {
-				Dictionary<int, bool> dict = (Dictionary<int, bool>)this;
-				return dict[i];
+	
+	public new bool this[string key] { 
+		get {
+			if (ContainsKey(key)) { 
+				Dictionary<string, bool> bs = this as Dictionary<string, bool>;
+				return bs[key];
 			}
 			return false;
 		}
 		
-		set {
-			if (ContainsKey(i)) {
-				Dictionary<int, bool> dict = (Dictionary<int, bool>)this;
-				dict[i] = value;
-			} else {
-				Add(i, value);
-			}
+		set { 
+			Dictionary<string, bool> bs = this as Dictionary<string, bool>;
+			bs[key] = value;
 		}
 	}
 	
-	public byte[] ToBytes() {
-		int max = maxIndex;
-		if (max == -1) { return new byte[0]; }
-		int size = (max / 8) + ((max % 8 == 0) ? 0 : 1);
-		byte[] array = new byte[size];
-		
-		for (int i = 0; i < (max+8); i += 8) {
-			int index = i / 8;
-			
-			byte b = 0;
-			for (int j = 0; j < 8; j++) {
-				if (this[i+j]) { b += (byte)(1 << j); }
+	public override string ToString() {
+		StringBuilder str = new StringBuilder();
+		foreach (var pair in this) {
+			if (pair.Value == true) {
+				str.Append(pair.Key);
+				str.Append(",");
 			}
-			
-			array[index] = b;
 		}
-		
-		return array;
+		return str.ToString();
 	}
 	
-	public void LoadBytes(byte[] bytes){ 
-		Clear();
-		
-		int index = 0;
-		for (int i = 0; i < bytes.Length; i++) {
-			byte b = bytes[i];
-			
-			for (int j = 7; j >= 0; j--) {
-				if (b > (1 << j)) {
-					b -= (byte)(1 << j);
-					this[index] = true;
-				}
-				
-				
-				index++;
-			}
-			
-			
-			
+	public void LoadString(string str) {
+		string[] keys = str.Split(',');
+		foreach (string key in keys) { 
+			if (key == "") { continue; }
+			this[key] = true; 
 		}
-		
-		
 	}
 	
 	public void Save(string key) {
-		
+		PlayerPrefs.SetString(key, ToString());
 	}
 	
 	public void Load(string key) {
-		
+		LoadString(PlayerPrefs.GetString(key));
 	}
 	
 }
